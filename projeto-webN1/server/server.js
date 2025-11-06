e4a8ceead9d5299559bcab269065332bb4d8fff4
Yasmin-Sousa <148486314+Yasmin-Sousa@users.noreply.github.com>
2025-11-06 16:56:36 -0300

    Set up Express server with routing and DB connection

--- projeto-webN1/server/server.js
+++ projeto-webN1/server/server.js
@@ -1,1 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./services/db');
+const authRoutes = require('./routes/auth');
+const postsRoutes = require('./routes/posts');
+const commentsRoutes = require('./routes/comments');
 
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+app.use('/auth', authRoutes);
+app.use('/posts', postsRoutes);
+app.use('/comments', commentsRoutes);
+
+
+const PORT = process.env.PORT || 4000;
+connectDB(process.env.MONGODB_URI || 'mongodb://localhost:27017/project_n1')
+.then(() => app.listen(PORT, () => console.log(`Server rodando na porta ${PORT}`)))
+.catch(err => console.error('Erro conectando ao DB:', err));
