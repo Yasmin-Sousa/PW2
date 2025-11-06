6043f4c829c2d9b0eca20ecc173da28869ab28e3
Yasmin-Sousa <148486314+Yasmin-Sousa@users.noreply.github.com>
2025-11-06 17:20:36 -0300

    Fetch and display posts with pagination

--- projeto-webN1/client/src/pages/AllPosts.jsx
+++ projeto-webN1/client/src/pages/AllPosts.jsx
@@ -1,1 +1,37 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import PostCard from "../components/PostCard";
+import Pagination from "../components/Pagination";
+
+function AllPosts() {
+  const [posts, setPosts] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/posts?page=${page}`)
+      .then((res) => {
+        setPosts(res.data.posts);
+        setTotalPages(res.data.totalPages);
+      })
+      .catch(() => alert("Erro ao carregar posts"));
+  }, [page]);
+
+  return (
+    <div>
+      <h2>Aqui Todos os Posts</h2>
+      {posts.map((p) => (
+        <PostCard key={p._id} post={p} />
+      ))}
+      <Pagination
+        currentPage={page}
+        totalPages={totalPages}
+        onPageChange={setPage}
+      />
+    </div>
+  );
+}
+
+export default AllPosts;
 
