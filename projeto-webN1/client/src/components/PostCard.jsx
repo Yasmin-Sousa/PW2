cab443e83c2954e8e76dc7551fa737d27d73b93e
Yasmin-Sousa <148486314+Yasmin-Sousa@users.noreply.github.com>
2025-11-06 17:18:03 -0300

    Update PostCard.jsx

--- projeto-webN1/client/src/components/PostCard.jsx
+++ projeto-webN1/client/src/components/PostCard.jsx
@@ -1,1 +1,16 @@
+import { Link } from "react-router-dom";
+
+function PostCard({ post }) {
+  return (
+    <div className="post-card">
+      <h3>{post.title}</h3>
+      <p>{post.text.slice(0, 120)}...</p>
+      <Link to={`/post/${post._id}`} className="btn">
+        Ler mais
+      </Link>
+    </div>
+  );
+}
+
+export default PostCard;
 
