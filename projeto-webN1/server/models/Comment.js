58e86674841910bf033a77cb73a3c0062c19e732
Yasmin-Sousa <148486314+Yasmin-Sousa@users.noreply.github.com>
2025-11-06 16:49:05 -0300

    Add Comment model with schema definition

--- projeto-webN1/server/models/Comment.js
+++ projeto-webN1/server/models/Comment.js
@@ -1,1 +1,7 @@
-
+const mongoose = require('mongoose');
+const CommentSchema = new mongoose.Schema({
+text: { type: String, required: true },
+postId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
+createdAt: { type: Date, default: Date.now }
+});
+module.exports = mongoose.model('Comment', CommentSchema);
